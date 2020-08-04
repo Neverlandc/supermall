25ae90c6b38728a448588ed01f28d90fe1648952
Neverlandc <1048271837@qq.com>
2020-08-04 17:51:46 +0800

    home页面

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -2,46 +2,192 @@
     <div id="home">
         <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
         <home-swiper :banners="banners"/>
+        <recommend-view :recommends="recommends"/>
+        <feature-view/>
+        <tab-control class="tab-control" :titles="['流行','精选','新款']"/>
+        <goods-list :goods="goods['pop'].list"/>
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
     </div>    
 </template>
 
 <script>
-    import NavBar from 'components/common/navbar/NavBar'
     import HomeSwiper from './childComps/HomeSwiper'
+    import RecommendView from './childComps/RecommendView'
+    import FeatureView from './childComps/FeatureView'
 
-    import {getHomeMultidata} from 'network/home'
+    import NavBar from 'components/common/navbar/NavBar'
+    import TabControl from 'components/content/tabControl/TabControl'
+    import GoodsList from 'components/content/goods/GoodsList'
+    
+
+    import {getHomeMultidata, getHomeGoods} from 'network/home'
     
 
     export default {
         name: 'Home',
         components: {
+            HomeSwiper,
+            RecommendView,
+            FeatureView,
             NavBar,
-            HomeSwiper
+            TabControl,
+            GoodsList
         },
         data() {
             return {
                 banners: [],
-                dKeywords: [],
-                keywords: [],
-                recommends: []
+                recommends: [],
+                goods: {
+                    'pop': {page: 0, list: []},
+                    'new': {page: 0, list: []},
+                    'sell': {page: 0, list: []}, 
+                }
             }
         },
         created() {
             // 1.请求多个数据
-            getHomeMultidata().then(res => {
-                // console.log(res)
-                this.banners = res.data.banner.list;
-                this.dKeywords = res.data.dKeyword.list;
-                this.keywords = res.data.keywords.list;
-                this.recommends = res.data.recommend.list;
-            })
+            this.getHomeMultidata()
+
+            // 2.请求商品数据
+            this.getHomeGoods('pop')
+            this.getHomeGoods('new')
+            this.getHomeGoods('sell')
+        },
+        methods: {
+            getHomeMultidata() {
+                getHomeMultidata().then(res => {
+                    // console.log(res)
+                    this.banners = res.data.banner.list;
+                    this.recommends = res.data.recommend.list;
+                })
+            },
+            getHomeGoods(type) {
+                const page = this.goods[type].page + 1
+                getHomeGoods(type, page).then(res => {
+                    this.goods[type].list.push(...res.data.list)
+                    this.goods[type].page ++
+                })
+            }
         }
     }
 </script>
 
 <style>
+    #home {
+        padding-top: 44px;
+    }
     .home-nav {
         background-color: var(--color-tint);
         color: #fff;
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        z-index: 5;
+    }
+    .tab-control {
+        position: sticky;
+        top: 44px;
     }
 </style>
